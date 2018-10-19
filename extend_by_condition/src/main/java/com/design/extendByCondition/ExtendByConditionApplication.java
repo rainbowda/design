dee2a9d5cb03531d24726542b0fbea874e88b48d
huangzd <930211680@qq.com>
2018-10-19 08:33:31 +0800

    根据条件进行扩展的设计项目

---
+++ extend_by_condition/src/main/java/com/design/extendByCondition/ExtendByConditionApplication.java
@@ -0,0 +1,12 @@
+package com.design.extendByCondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendByConditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtendByConditionApplication.class, args);
+    }
+}
