549345de4db59b9cffd0b3bb9219b98db4de75a5
huangzd <930211680@qq.com>
2018-10-20 10:22:35 +0800

    路由测试类

--- extend_by_condition/src/test/java/com/design/extendByCondition/ApplicationTests.java
+++ extend_by_condition/src/test/java/com/design/extendByCondition/ApplicationTests.java
@@ -1,16 +1,23 @@
 package com.design.extendByCondition;
 
+import com.design.extendByCondition.bean.SupportBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExtendByConditionApplicationTests {
+public class ApplicationTests {
+
+    @Autowired
+    private RouterHelper routerHelper;
+
 
     @Test
     public void contextLoads() {
+        routerHelper.execute(new SupportBean(3));
     }
 
 }
