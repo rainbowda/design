135a8a557f8a6b7a754cc3611b8da88f9e191f10
huangzd <930211680@qq.com>
2018-10-20 10:21:31 +0800

    bean

---
+++ extend_by_condition/src/main/java/com/design/extendByCondition/bean/SupportBean.java
@@ -0,0 +1,18 @@
+package com.design.extendByCondition.bean;
+
+public class SupportBean {
+
+    private Integer supportNum;
+
+    public Integer getSupportNum() {
+        return supportNum;
+    }
+
+    public void setSupportNum(Integer supportNum) {
+        this.supportNum = supportNum;
+    }
+
+    public SupportBean(Integer supportNum) {
+        this.supportNum = supportNum;
+    }
+}
